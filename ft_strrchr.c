46012d8a75c818204390ee8dcbd2a83686b2ad7e
Wendy Rosmery Corzo Altuve <wecorzo-@c3r7s4.42madrid.com>
2023-01-26 18:27:44 +0100

    push

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,81 @@
 /*   By: wecorzo- <wecorzo-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:36:27 by wecorzo-          #+#    #+#             */
-/*   Updated: 2023/01/25 18:28:22 by wecorzo-         ###   ########.fr       */
+/*   Updated: 2023/01/26 16:29:48 by wecorzo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*str;
-	char	cc;
 	size_t	i;
 
-	str = (char *)s;
-	cc = (char)c;
-	i = ft_strlen(s);
-	while (i-- >= 0)
+	i = ft_strlen(s) + 1;
+	while (i > 0)
 	{
-		if (str[i] == cc)
-			return (&str[i]);
+		i--;
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
 	}
 	return (NULL);
 }
+//
+//char	*ft_strrchr(const char *s, int c)
+//{
+//	size_t	i;
+//
+//	i = ft_strlen(s) + 1;
+//	while (i > 0)
+//	{
+//		i--;
+//		if (s[i] == (char)c)
+//			return (&((char *)s)[i]);
+//	}
+//	//if (s[0] == (char)c)
+//	//	return (&((char *)s)[0]);
+//	return (NULL);
+//}
+
+//char	*ft_strrchr(const char *s, int c)
+//{
+//	size_t	i;
+//	char	*reverse_str;
+//
+//	i = ft_strlen(s);
+//	reverse_str = (char *)s + i;
+//	while (reverse_str >= s)
+//	{
+//		if (*reverse_str == (char)c)
+//			return (((char *)reverse_str));
+//		reverse_str--;
+//	}
+//	return (NULL);
+//}
+//char	*ft_strrchr(const char *s, int c)
+//{
+//	char	*last_match;
+//
+//	last_match = NULL;
+//	while (*s != '\0')
+//	{
+//		if (*s == (char)c)
+//			last_match = (char *)s;
+//		s++;
+//	}
+//	if ((char)c == '\0')
+//		last_match = (char *)s;
+//	if (last_match == NULL)
+//		return NULL;
+//	return last_match;
+//}
+///*
+//#include <stdio.h>
+//int main()
+//{
+//	const	char *s = "bonjourno";
+//	int	c = 'b';
+//	printf("%s\n", strrchr(s + 2, c));
+//	printf("%s", ft_strrchr(s + 2 , c));
+//	return (0);
+//}
+//*/
